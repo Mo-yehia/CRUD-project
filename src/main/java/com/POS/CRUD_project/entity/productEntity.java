e3330240e2d79431a71283325b8a3c51f8ed30d1
Mohamed Yehia <122978317+Mo-yehia@users.noreply.github.com>
2024-12-16 14:41:09 +0200

    Update productEntity.java

--- src/main/java/com/POS/CRUD_project/entity/productEntity.java
+++ src/main/java/com/POS/CRUD_project/entity/productEntity.java
@@ -2,11 +2,10 @@ package com.POS.CRUD_project.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.Data;
+
 
 
 @Entity
-@Data
 public class productEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +21,28 @@ public class productEntity {
     @Pattern(regexp = "\\d+", message = "Barcode must contain only digits")
     private String productBarcode;
 
+    public @NotBlank(message = "Product name is required") String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(@NotBlank(message = "Product name is required") String productName) {
+        this.productName = productName;
+    }
+
+    public @NotNull(message = "Price is required") @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero") Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(@NotNull(message = "Price is required") @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero") Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public @Pattern(regexp = "\\d+", message = "Barcode must contain only digits") String getProductBarcode() {
+        return productBarcode;
+    }
+
+    public void setProductBarcode(@Pattern(regexp = "\\d+", message = "Barcode must contain only digits") String productBarcode) {
+        this.productBarcode = productBarcode;
+    }
 }
 
